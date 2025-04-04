affcc1b10ce798c2c5f0248f38098affc5f8ee6e
diogo <diogo.lima.flooxai@gmail.com>
2025-04-04 08:08:28 -0300

    feat: add EJS Embedded to display data from database and EJS include to templates

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
+var conn = require("./../inc/db");
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  conn.query(`SELECT * FROM tb_menus ORDER BY title`, (err, results) => {
+    if (err) {
+      console.log(err);
+    }
+
+    res.render("index", { 
+      title: "Restaurante Saboroso!",
+      menus: results
+    });
+  });
 });
+
+router.get('/contacts', function(req, res, next){
+  res.render('contacts', {
+    title:'Contato - Restaurante Saboroso!'
+  })
+});
+
+router.get('/menus', function(req, res, next){
+  res.render('menus', {
+    title:'Menus - Restaurante Saboroso!'
+  })
+});
+
+router.get('/reservations', function(req, res, next){
+  res.render('reservations', {
+    title:'Reservas - Restaurante Saboroso!'
+  })
+});
+
+router.get('/services', function(req, res, next){
+  res.render('services', {
+    title:'Services - Restaurante Saboroso!'
+  })
+});
+
 
 module.exports = router;
