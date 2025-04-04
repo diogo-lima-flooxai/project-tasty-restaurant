88b75647ba3626b431258969d26c4bdaa3555a56
diogo <diogo.lima.flooxai@gmail.com>
2025-04-04 08:28:01 -0300

    feat: add EJS includes to header

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -18,25 +18,33 @@ router.get("/", function (req, res, next) {
 
 router.get('/contacts', function(req, res, next){
   res.render('contacts', {
-    title:'Contato - Restaurante Saboroso!'
+    title:'Contato - Restaurante Saboroso!',
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga um oi!'
   })
 });
 
 router.get('/menus', function(req, res, next){
   res.render('menus', {
-    title:'Menus - Restaurante Saboroso!'
+    title:'Menus - Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1: 'Saboreie nosso menu!'
   })
 });
 
 router.get('/reservations', function(req, res, next){
   res.render('reservations', {
-    title:'Reservas - Restaurante Saboroso!'
+    title:'Reservas - Restaurante Saboroso!',
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma Mesa!'
   })
 });
 
 router.get('/services', function(req, res, next){
   res.render('services', {
-    title:'Services - Restaurante Saboroso!'
+    title:'Services - Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer poder servir!'
   })
 });
 
