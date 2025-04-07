9447dfdff62d4cc65af7c9e9628c2892072b17f9
diogo <diogo.lima.flooxai@gmail.com>
2025-04-07 09:06:12 -0300

    feat: querying user data with MySQL

--- saboroso/inc/admin.js
+++ saboroso/inc/admin.js
@@ -1,59 +1,79 @@
+var conn = require("./db")
 
 module.exports = {
-  
-  getParams(req, params){
-    return Object.assign({}, {
-      menus: req.menus,
-      user: req.session.user
-    }, params);                        
+  dashboard() {
+    return new Promise((resolve, reject) => {
+      conn.query(`
+        SELECT
+          (SELECT COUNT(*) FROM tb_contacts) AS nrcontacts,
+          (SELECT COUNT(*) FROM tb_menus) AS nrmenus,
+          (SELECT COUNT(*) FROM tb_reservations) AS nrreservations,
+          (SELECT COUNT(*) FROM tb_users) AS nrusers;
+        `, (err, results) => {
+          if(err) {
+            reject(err);
+          } else {
+            resolve(results[0])
+          }
+        });
+    });
   },
-  
-  getMenus(req){
+
+  getParams(req, params) {
+    return Object.assign(
+      {},
+      {
+        menus: req.menus,
+        user: req.session.user,
+      },
+      params
+    );
+  },
+
+  getMenus(req) {
     let menus = [
       {
         text: "Tela Inicial",
         href: "/admin/",
         icon: "home",
-        active: false
+        active: false,
       },
       {
         text: "Menu",
         href: "/admin/menus",
         icon: "cutlery",
-        active: false
+        active: false,
       },
       {
         text: "Reservas",
         href: "/admin/reservations",
         icon: "calendar-check-o",
-        active: false
+        active: false,
       },
       {
         text: "Contatos",
         href: "/admin/contacts",
         icon: "comments",
-        active: false
+        active: false,
       },
       {
         text: "Usuários",
         href: "/admin/users",
         icon: "users",
-        active: false
+        active: false,
       },
       {
         text: "E-mails",
         href: "/admin/emails",
         icon: "envelope",
-        active: false
-      }
-    ]
+        active: false,
+      },
+    ];
 
-    menus.map(menu =>{
-
-      if(menu.href === `/admin${req.url}`) menu.active = true;
-
-    })
+    menus.map((menu) => {
+      if (menu.href === `/admin${req.url}`) menu.active = true;
+    });
 
     return menus;
-  }
-}
+  },
+};
