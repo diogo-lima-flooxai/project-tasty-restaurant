26ba4e0d645bfb632f9e34cf8f3caabe64fa0af6
diogo <diogo.lima.flooxai@gmail.com>
2025-04-07 10:29:07 -0300

    feat: MENU - add new menus, sending in POST method and changing and querying in MySQL

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var users = require("./../inc/users");
-var admin = require("./../inc/admin")
+var admin = require("./../inc/admin");
+var menus = require("./../inc/menus");
 var router = express.Router();
 
 router.use(function(req, res, next){
@@ -68,8 +69,22 @@ router.get("/emails", function(req, res, next){
 });
 
 router.get("/menus", function(req, res, next){
-  res.render('admin/menus', admin.getParams(req));
+
+  menus.getMenus().then(data =>{
+    res.render('admin/menus', admin.getParams(req, {
+      data
+    }));
+  })
+
 });
+
+router.post("/menus", function(req, res, next){
+  menus.save(req.fields, req.files).then(results=>{
+    res.send(results)
+  }).catch(err =>{
+    res.send(err);
+  })
+})
 
 router.get("/reservations", function(req, res, next){
   res.render('admin/reservations', admin.getParams(req, {
