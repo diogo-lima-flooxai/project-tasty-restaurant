d7b95693918438d16af662e07c137ff9a7bc14f9
diogo <diogo.lima.flooxai@gmail.com>
2025-04-08 09:05:59 -0300

    feat: Reservation - add Create

--- saboroso/inc/reservations.js
+++ saboroso/inc/reservations.js
@@ -14,20 +14,42 @@ module.exports = {
   save(fields){
     return new Promise((resolve, reject) => {
 
-      let date = fields.date.split('/');
+      if(fields.date.indexOf('/') > -1){
+        
+        let date = fields.date.split('/');
+        fields.date = `${date[2]}-${date[1]}-${date[0]}`;
+      }
 
-      fields.date = `${date[2]}-${date[1]}-${date[0]}`;
+      let query, params =  [
+        fields.name,
+        fields.email,
+        fields.people,
+        fields.date,
+        fields.time
+      ]
 
-      conn.query(`
-        INSERT INTO tb_reservations (name, email, people, date, time)
-        VALUES(?, ?, ?, ?, ?)
-        `, [
-          fields.name,
-          fields.email,
-          fields.people,
-          fields.date,
-          fields.time
-        ],(err, results) =>{
+      if (parseInt(fields.id) > 0){
+
+        query = `
+          UPDATE tb_reservations
+          SET
+            name = ?,
+            email = ?,
+            people = ?,
+            date = ?,
+            time = ?
+          WHERE id = ?
+        `;
+        params.push(fields.id);
+
+      } else {
+        query = `
+          INSERT INTO tb_reservations (name, email, people, date, time)
+          VALUES(?, ?, ?, ?, ?)
+        `
+      }
+
+      conn.query(query, params, (err, results) =>{
           if(err){
             reject(err);
           } else {
