b50c1f2dc4cc49db673a893133f1bc197d982b94
diogo <diogo.lima.flooxai@gmail.com>
2025-04-09 09:37:56 -0300

    feat: add navigation buttons to pagination

--- saboroso/inc/reservations.js
+++ saboroso/inc/reservations.js
@@ -1,5 +1,5 @@
 var conn = require("./db");
-var Pagination = require('./Pagination')
+var Pagination = require("./Pagination");
 
 module.exports = {
   render(req, res, error, success) {
@@ -57,15 +57,35 @@ module.exports = {
     });
   },
 
-  getReservations(page) {
-    if (!page) page = 1;
+  getReservations(req) {
+    return new Promise((resolve, reject) => {
+      let page = req.query.page;
+      let dtstart = req.query.start;
+      let dtend = req.query.end;
 
-    let pag = new Pagination(
-      `
-      SELECT SQL_CALC_FOUND_ROWS * FROM tb_reservations ORDER BY name LIMIT ?, ?
-      `
-    );
-    return pag.getPage(page);
+      if (!page) page = 1;
+
+      let params = [];
+
+      if (dtstart && dtend) params.push(dtstart, dtend);
+
+      let pag = new Pagination(
+        `
+        SELECT SQL_CALC_FOUND_ROWS * 
+        FROM tb_reservations 
+        ${dtstart && dtend ? "WHERE date BETWEEN ? AND ?" : ""} 
+        ORDER BY name LIMIT ?, ?
+        `,
+        params
+      );
+
+      pag.getPage(page).then((data) => {
+        resolve({
+          data,
+          links: pag.getNavigation(req.query),
+        });
+      });
+    });
   },
 
   delete(id) {
